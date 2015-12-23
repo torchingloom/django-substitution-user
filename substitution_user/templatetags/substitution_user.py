a3c8aaec6fc374b29de355432a8f805ec797d3b3
Alexey Baranov <atlantij@gmail.com>
2015-12-23 15:29:08 +0300

    2nd django < 1.7 support

--- substitution_user/templatetags/substitution_user.py
+++ substitution_user/templatetags/substitution_user.py
@@ -7,4 +7,7 @@ register = template.Library()
 @register.assignment_tag(takes_context=True)
 @register.simple_tag(takes_context=True)
 def substitution_user_get_real_user(context):
-    return getattr(context.request, SUBSTITUTION_USER_REQUEST_REAL_USER_KEY, context.request.user)
+    try:
+        return getattr(context.request, SUBSTITUTION_USER_REQUEST_REAL_USER_KEY, context.request.user)
+    except AttributeError as e:
+        return getattr(context['request'], SUBSTITUTION_USER_REQUEST_REAL_USER_KEY, context['request'].user)
